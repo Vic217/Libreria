b95f68cc68b158c369dfd4dd2721a39259fa13cd
VicGar21 <victorgarp15@hotmail.com>
2023-08-31 14:29:01 -0600

    Se agrega constructor de objetos

---
+++ script.js
@@ -0,0 +1,17 @@
+// Variables
+const miLibreria = [];
+
+// Constructor de Objetos
+function Libro(titulo, autor, paginas, leido) {
+    this.titulo = titulo;
+    this.autor = autor;
+    this.paginas = paginas;
+    this.leido = leido;
+    this.info = function () {
+        return titulo + ", " + autor + ", " + paginas + ", " + leido;
+    };
+}
+
+function agregarLibroALibreria(nuevo) {
+    miLibreria.push(nuevo);
+}
