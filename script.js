eb7d1a09cfc98044114ae9e88e1337820798984c
VicGar21 <victorgarp15@hotmail.com>
2023-09-02 09:59:53 -0600

    Agrego datos a tabla y doy funcionamiento a los botones

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 // Variables
 const agregarNuevo = document.getElementById("agregar_nuevo");
 const dialogo = document.getElementById("dialogo");
-const cerrarFormulario = document.getElementById("cerrar");
+const cerrar = document.getElementById("cerrar");
+const añadir = document.getElementById("añadir");
+const formulario = document.getElementById("formulario");
+const titulo = document.getElementById("titulo");
+const autor = document.getElementById("autor");
+const paginas = document.getElementById("paginas");
+const leido = document.getElementById("leido");
+const cuerpo = document.getElementById("cuerpo");
+const vacia = document.getElementById("vacia");
 const miLibreria = [];
-
-agregarNuevo.addEventListener("click", () => {
-    dialogo.showModal(); // Abre el cuadro de diálogo
-});
-
-cerrarFormulario.addEventListener("click", () => {
-    dialogo.close(); // Cierra el cuadro de diálogo
-});
 
 // Constructor de Objetos
 function Libro(titulo, autor, paginas, leido) {
@@ -18,17 +18,105 @@ function Libro(titulo, autor, paginas, leido) {
     this.autor = autor;
     this.paginas = paginas;
     this.leido = leido;
-    this.info = function () {
-        return titulo + ", " + autor + ", " + paginas + ", " + leido;
-    };
 }
+
+Libro.prototype.cambiarLeido = function() {
+    this.leido = !this.leido;
+}
+
+function cambiarLeido(indice) {
+    miLibreria[indice].cambiarLeido();
+
+    recorrer();
+}
+
+function eliminar_libro(indice) {
+    miLibreria.splice(indice, 1);
+    if (miLibreria.length === 0){
+        const con_datos = document.getElementById("datos");
+        con_datos.removeAttribute("id");
+        con_datos.setAttribute("id", "vacia");
+    }
+    recorrer();
+}
+
+// Recorrer la lista
+function recorrer() {
+    cuerpo.innerHTML = "";
+
+    for (let i = 0; i < miLibreria.length; i++){
+        const nueva_fila = document.createElement("tr");
+        nueva_fila.setAttribute("class", "nueva");
+        const celdaT = document.createElement("td");
+        const celdaA = document.createElement("td");
+        const celdaP = document.createElement("td");
+        const celdaL = document.createElement("td");
+        const botonLeido = document.createElement("button");
+        const eliminar = document.createElement("td");
+        const botonEliminar = document.createElement("button");
+
+        celdaT.textContent = miLibreria[i].titulo;
+        celdaA.textContent = miLibreria[i].autor;
+        celdaP.textContent = miLibreria[i].paginas;
+
+        botonLeido.setAttribute("class", "estado");
+        let estado = miLibreria[i].leido ? "Si lo he leído" : "No lo he leído";
+        botonLeido.textContent = estado;
+        miLibreria[i].leido ? botonLeido.setAttribute("id", "si") : botonLeido.setAttribute("id", "no");
+        celdaL.appendChild(botonLeido);
+        botonEliminar.textContent = "eliminar";
+        eliminar.appendChild(botonEliminar);
+        botonEliminar.setAttribute("id", i);
+
+        nueva_fila.appendChild(celdaT);
+        nueva_fila.appendChild(celdaA);
+        nueva_fila.appendChild(celdaP);
+        nueva_fila.appendChild(celdaL);
+        nueva_fila.appendChild(eliminar);
+        cuerpo.appendChild(nueva_fila);
+
+        botonEliminar.addEventListener("click", () => {
+            eliminar_libro(i);
+        });
+
+        botonLeido.addEventListener("click", () => {
+            cambiarLeido(i);
+        })
+    }
+}
+
+agregarNuevo.addEventListener("click", (e) => {
+    e.preventDefault();
+    dialogo.showModal(); // Abre el cuadro de diálogo
+});
+
+// Agrega el libro a tabla
+añadir.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    // Verifica si los campos del formulario están llenos
+    if (titulo.value && autor.value && paginas.value) {
+        const nuevo_libro = new Libro(titulo.value, autor.value, paginas.value, leido.checked);
+        agregarLibroALibreria(nuevo_libro);
+        formulario.reset();
+        dialogo.close();
+    } else {
+        alert("Por favor, complete todos los campos antes de agregar el libro.");
+    }
+});
+
+cerrar.addEventListener("click", (e) => {
+    e.preventDefault();
+    formulario.reset();
+    dialogo.close(); // Cierra el cuadro de diálogo
+});
 
 function agregarLibroALibreria(nuevo) {
     miLibreria.push(nuevo);
-}
+    if (miLibreria.length > 0) {
+        vacia.removeAttribute("id");
+        vacia.setAttribute("id", "datos");
+    }
 
-function recorrer(miLibreria) {
-    miLibreria.forEach(element => {
-        console.log(element);
-    });
+    recorrer();
 }
